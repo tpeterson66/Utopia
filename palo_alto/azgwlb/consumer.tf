1dfc8f1de133062f81e6de7cd511c023db308183
Tom Peterson <tpeterson@Toms-Mac-mini.home.tpeterson.io>
2022-09-12 15:15:12 -0400

    updates to the egress config files

--- palo_alto/azgwlb/consumer.tf
+++ palo_alto/azgwlb/consumer.tf
@@ -96,3 +96,26 @@ resource "azurerm_linux_virtual_machine" "consumer" {
   }
 }
 
+resource "azurerm_network_security_group" "consumer" {
+  name                = "consumer-vm-nsg"
+  location            = azurerm_resource_group.consumer.location
+  resource_group_name = azurerm_resource_group.consumer.name
+
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix    = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "consumer" {
+  network_interface_id      = azurerm_network_interface.consumer.id
+  network_security_group_id = azurerm_network_security_group.consumer.id
+}
--- palo_alto/azgwlb/vnet.tf
+++ palo_alto/azgwlb/vnet.tf
@@ -14,6 +14,10 @@ variable "data_subnet" {
   type        = list(string)
   description = "Address prefix for the data network"
 }
+variable "management_public_ips" {
+  description = "Public IP address to manage the firewalls"
+  type        = list(string)
+}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
@@ -47,8 +51,19 @@ resource "azurerm_network_security_group" "mgmt" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
+    destination_port_range     = "22"
+    source_address_prefixes      = var.management_public_ips
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "https"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefixes      = var.management_public_ips
     destination_address_prefix = "*"
   }
 }
